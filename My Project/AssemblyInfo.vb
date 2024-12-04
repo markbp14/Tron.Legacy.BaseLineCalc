99073ecc734fac77c433991c7943ae264abf8562
peugmar <peugmar@chrobinson.com>
2024-12-04 13:31:31 -0600

    Add project files.

---
+++ My Project/AssemblyInfo.vb
@@ -0,0 +1,32 @@
+﻿Imports System
+Imports System.Reflection
+Imports System.Runtime.InteropServices
+
+' General Information about an assembly is controlled through the following
+' set of attributes. Change these attribute values to modify the information
+' associated with an assembly.
+
+' Review the values of the assembly attributes
+
+<Assembly: AssemblyTitle("Tron.Legacy.BaseLineCalc")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("C.H. Robinson")>
+<Assembly: AssemblyProduct("Tron.Legacy.BaseLineCalc")>
+<Assembly: AssemblyCopyright("Copyright © C.H. Robinson 2024")>
+<Assembly: AssemblyTrademark("")>
+
+<Assembly: ComVisible(False)>
+
+'The following GUID is for the ID of the typelib if this project is exposed to COM
+<Assembly: Guid("dddb6803-8c27-4898-94e2-44c16ac1d90e")>
+
+' Version information for an assembly consists of the following four values:
+'
+'      Major Version
+'      Minor Version
+'      Build Number
+'      Revision
+'
+
+<Assembly: AssemblyVersion("1.0.0.0")>
+<Assembly: AssemblyFileVersion("1.0.0.0")>
